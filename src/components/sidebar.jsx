ea7e551ae33da7702c7a747a6ee0fafa1abbe68b
Viasheslav Rashnik <vasheslav1000@gmail.com>
2023-09-14 01:22:34 +0300

    fix

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -6,16 +6,38 @@ const Sidebar = () => {
       <form className={css.form}>
         <label className={css.label}>
           Car brand
-          <input type="text" name="Car_brand" className={css.car_brend} />
+          <input
+            type="text"
+            name="Car_brand"
+            placeholder="Enter the text"
+            className={css.car_brend}
+          />
         </label>
         <label className={css.label}>
           Price/ 1 hour
-          <input type="text" name="Price" className={css.price} />
+          <input
+            type="text"
+            name="Price"
+            placeholder="To $"
+            className={css.price}
+          />
         </label>
         <label className={css.label}>
           Сar mileage / km
-          <input type="text" name="From_mileage" className={css.mileage} />
-          <input type="text" name="To_mileage" className={css.mileage} />
+          <div className={css.box}>
+            <input
+              type="text"
+              name="From_mileage"
+              placeholder=""
+              className={css.mileage1}
+            />
+            <input
+              type="text"
+              name="To_mileage"
+              placeholder=""
+              className={css.mileage2}
+            />
+          </div>
         </label>
         <input type="submit" value="Search" className={css.button} />
       </form>
